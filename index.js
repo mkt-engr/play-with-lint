be0453c6bb0af42696cced19230a6c377936486f
Makito Mori <mkt-engr@gmail.com>
2023-01-21 16:10:43 +0900

    中括弧の制限

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ console.log(`ESLint`);
 console.log(`ESLint${aaa}`);
 sub();
 aaa === bbb;
+
+if (aaa === bbb) {
+  console.log("object");
+  console.log("object");
+}
+
+if (aaa === bbb) {
+  console.log("object");
+}
